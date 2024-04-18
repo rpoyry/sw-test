767154a3fa400c28a7adc61403a828afa55cc0e9
rpoyry <roope.poyry@easyopp.fi>
2024-04-18 09:21:44 +0300

    Edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited <code>src/App.tsx</code> and saved to reload.
+          Edit <code>src/App.tsx</code> and saved to reload.
         </p>
         <a
           className="App-link"
