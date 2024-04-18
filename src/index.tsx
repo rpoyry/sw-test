a46e666c88cf95dd8f0141a3e7ae8df420e7d6bc
rpoyry <roope.poyry@easyopp.fi>
2024-04-18 09:28:51 +0300

    Fix build

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -14,15 +14,15 @@ root.render(
   </React.StrictMode>
 );
 
-const onUpdate = (registration: ServiceWorkerRegistration) => {
-  // Display a notification or prompt the user to update the application
-  if (window.confirm('A new version of the application is available. Do you want to update?')) {
-    // Send a message to the service worker to skip waiting and activate the new version
-    registration.waiting?.postMessage({ type: 'SKIP_WAITING' });
-    // Refresh the page to activate the new version
-    window.location.reload();
-  }
-};
+// const onUpdate = (registration: ServiceWorkerRegistration) => {
+//   // Display a notification or prompt the user to update the application
+//   if (window.confirm('A new version of the application is available. Do you want to update?')) {
+//     // Send a message to the service worker to skip waiting and activate the new version
+//     registration.waiting?.postMessage({ type: 'SKIP_WAITING' });
+//     // Refresh the page to activate the new version
+//     window.location.reload();
+//   }
+// };
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
