50a3fa90906f13664402f3a0ff832568de06dc90
rpoyry <roope.poyry@easyopp.fi>
2024-04-18 10:08:25 +0300

    Try self-destruct

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 (function (self) {
-    var versionNo = '5'; //VersionNo
+    var versionNo = '6'; //VersionNo
     //Service worker contents...
     // self.addEventListener("install", function (event) {
     //     console.log("[Service Worker] Installing Service Worker...");
@@ -13,28 +13,42 @@
     //         self.skipWaiting();
     //     }
     // });
-    self.addEventListener('install', function(e) {
-        self.skipWaiting();
-      });
+    // self.addEventListener('install', function(e) {
+    //     self.skipWaiting();
+    //   });
       
-      self.addEventListener('activate', function(e) {
-        e.waitUntil(self.clients.claim());
+    //   self.addEventListener('activate', function(e) {
+    //     e.waitUntil(self.clients.claim());
 
-        self.clients.matchAll().then((clients) => {
-            clients.forEach((client) => {
-              client.postMessage({ type: 'UPDATE_AVAILABLE' });
-            });
-          });
+    //     self.clients.matchAll().then((clients) => {
+    //         clients.forEach((client) => {
+    //           client.postMessage({ type: 'UPDATE_AVAILABLE' });
+    //         });
+    //       });
 
-        // self.registration.unregister()
-        //   .then(function() {
-        //     return self.clients.matchAll();
-        //   })
-        //   .then(function(clients) {
-        //     clients.forEach(client => client.navigate(client.url))
-        //   });
-      });
-    // self.addEventListener("fetch", function (event) {
-    // });
+    //     // self.registration.unregister()
+    //     //   .then(function() {
+    //     //     return self.clients.matchAll();
+    //     //   })
+    //     //   .then(function(clients) {
+    //     //     clients.forEach(client => client.navigate(client.url))
+    //     //   });
+    //   });
+    // // self.addEventListener("fetch", function (event) {
+    // // });
+
+    self.addEventListener('install', function(e) {
+      self.skipWaiting();
+    });
+    
+    self.addEventListener('activate', function(e) {
+      self.registration.unregister()
+        .then(function() {
+          return self.clients.matchAll();
+        })
+        .then(function(clients) {
+          clients.forEach(client => client.navigate(client.url))
+        });
+    });
 })(self);
 //# sourceMappingURL=sw.js.map
