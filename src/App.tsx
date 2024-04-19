69252a50b25a69ae843d36fc6b98b3b1c87f9e59
rpoyry <roope.poyry@easyopp.fi>
2024-04-19 15:15:49 +0300

    Fix App

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
           Canceling the <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"asdsa
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -22,6 +22,4 @@ function App() {
     </div>
   );
 }
-asdsa
 export default App;
-ssasdsasdasdasdassdd
